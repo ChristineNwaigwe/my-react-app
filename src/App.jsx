387340cdb540dd6bed7df1348e98bba79ff5bd57
Christine Nwaigwe <c.i.nwaigwe@wustl.edu>
2025-04-01 19:33:17 -0500

    defining the handleClick function inside of board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>);
 }
 
 
@@ -10,12 +16,13 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-      <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[0]} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
       <div className="board-row">
-      <Square value={squares[3]} />
+        <Square value={squares[3]} />
         <Square value={squares[4]} />
         <Square value={squares[5]} />
       </div>
